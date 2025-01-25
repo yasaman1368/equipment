4c5edb33caed254a5edf72a2cd41130fd4c4d3fd
hossein aalipour <alipor.hossain@gmial.com>
2025-01-25 21:29:29 +0330

    quran alfajr show member ageGroup added

--- wp-content/plugins/ghoran-alfajr-registration/_inc/show-members/show_members.php
+++ wp-content/plugins/ghoran-alfajr-registration/_inc/show-members/show_members.php
@@ -5,26 +5,26 @@ function show_members()
     global $wpdb;
     $table = $wpdb->prefix   . 'fajr_qoran';
     $members = $wpdb->get_results("SELECT  * FROM $table ");
-    if ($members):
+
 ?>
 
-        <!DOCTYPE html>
-        <html lang="fa" dir="rtl">
+    <!DOCTYPE html>
+    <html lang="fa" dir="rtl">
 
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title><?php wp_title(); ?></title>
-            <link href="<?php echo get_template_directory_uri() ?>/assets/images/PS.png" rel="shortcut icon" />
-            <!-- Bootstrap CSS -->
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
-            <!-- Bootstrap Icons -->
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-        </head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php wp_title(); ?></title>
+        <link href="<?php echo get_template_directory_uri() ?>/assets/images/PS.png" rel="shortcut icon" />
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
+        <!-- Bootstrap Icons -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    </head>
 
-        <body class="bg-light">
-
+    <body class="bg-light">
+        <?php if ($members): ?>
             <div class="container mt-5">
                 <h2 class="text-center">جدول ثبت نام مسابقات قرآنی</h2>
                 <div class="table-responsive">
@@ -37,6 +37,7 @@ function show_members()
                                 <th>کد ملی</th>
                                 <th>شماره موبایل</th>
                                 <th>جنسیت</th>
+                                <th>گروه سنی</th>
                                 <th>رشته مسابقه</th>
                                 <th>شهر</th>
                                 <th>آدرس</th>
@@ -46,7 +47,11 @@ function show_members()
                         <tbody>
                             <?php
                             $i = 1;
-                            foreach ($members as $member): ?>
+
+                            foreach ($members as $member):
+                                $age_group = $member->age;
+                                $age_group = $age_group === 'adults' ? 'بزرگسال' : ($age_group === 'teenagers' ? 'نوجوان' : 'نونهال');
+                            ?>
                                 <tr>
                                     <td> <?php echo $i++ ?></td>
                                     <td><?= htmlspecialchars($member->name) ?></td>
@@ -54,6 +59,7 @@ function show_members()
                                     <td><?= htmlspecialchars($member->national_num) ?></td>
                                     <td><?= htmlspecialchars($member->phone) ?></td>
                                     <td><?= htmlspecialchars($member->gender === 'male' ? 'مرد' : 'زن') ?></td>
+                                    <td><?= htmlspecialchars($age_group) ?></td>
                                     <td><?= htmlspecialchars($member->field) ?></td>
                                     <td><?= htmlspecialchars($member->city) ?></td>
                                     <td><?= htmlspecialchars($member->address) ?></td>
@@ -67,12 +73,17 @@ function show_members()
                     </table>
                 </div>
             </div>
-        </body>
 
-        </html>
+        <?php else :  ?>
+            <div class="alert alert-info p-3 mt-5 fw-bolder shadow mx-auto w-75 text-center">
+                تاکنون هیچ ثبت نامی انجام نشده است!!!
+            </div>
+        <?php endif ?>
+    </body>
+
+    </html>
 
 <?php
-    endif;
 }
 add_shortcode('show-members', 'show_members');
 ?>
