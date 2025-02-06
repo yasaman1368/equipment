6f8efe00b60aca8b8a11dd197c8a99a5014f83b4
hossein aalipour <alipor.hossain@gmial.com>
2025-02-06 09:16:21 +0330

    clean quranlafajr codes

--- wp-content/themes/math789/Q-alfajr.php
+++
@@ -1,9 +0,0 @@
-<?php /* Template Name: quran-alfajr */
-
-if (!empty($_GET['submit']) && $_GET['submit'] === 'QF') {
-    do_shortcode('[fajr]');
-} elseif (!empty($_GET['admin']) && $_GET['admin'] === 'mohammadhosseinbahrani6607') {
-    do_shortcode('[show-members]');
-} else {
-    do_shortcode('[quran-alfajr-home]');
-}
