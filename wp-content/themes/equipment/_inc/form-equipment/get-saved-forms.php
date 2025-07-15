67bc017a01c7c744c191ee33753c71ae3272bf26
hossein aalipour <alipor.hossain@gmial.com>
2025-07-15 15:15:12 +0330

    refac: get saved form based on user's locations

--- wp-content/themes/equipment/_inc/form-equipment/get-saved-forms.php
+++ wp-content/themes/equipment/_inc/form-equipment/get-saved-forms.php
@@ -1,11 +1,39 @@
 <?php
 
-function get_saved_forms()
-{
+/**
+ * Retrieves saved forms accessible to the current user based on their locations
+ *
+ * @return void Sends JSON response
+ */
+function get_saved_forms(){
     global $wpdb;
+    
     $table_name_forms = $wpdb->prefix . 'equipment_forms';
 
-    $forms = $wpdb->get_results("SELECT id, form_name FROM $table_name_forms", ARRAY_A);
+    $current_user_id = get_current_user_id();
+    $current_user_locations = get_user_locations($current_user_id);
+
+    $forms_rows = $wpdb->get_results("SELECT id, form_name,locations FROM $table_name_forms", ARRAY_A);
+    $forms = [];
+
+    foreach ($forms_rows as $row) {
+
+        $locations = json_decode($row['locations']);
+
+        if (is_array($locations)) {
+
+            foreach ($current_user_locations as $location) {
+
+                if (array_search($location, $locations) !== false) {
+                    array_push($forms, $row);
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+
+
 
     $status = empty($forms) ? 'empty' : 'hasform';
 
