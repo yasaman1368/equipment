51723d22c664b000057efff80159cb7aedc879db
hossein aalipour <alipor.hossain@gmial.com>
2025-07-30 16:51:06 +0330

    feat: display workfolw for users

--- wp-content/themes/equipment/view/panel/WorkflowView.php
+++ wp-content/themes/equipment/view/panel/WorkflowView.php
@@ -1,92 +1,113 @@
-<div class="container ">
-<h1>workflow</h1>
-    <div class="section_our_solution mt-5" dir="rtl">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <div class="our_solution_category">
-                    <div class="solution_cards_box">
-                        <div class="solution_card">
-                            <div class="hover_color_bubble"></div>
-                            <div class="so_top_icon">
-                                <!-- Add icon here if needed -->
-                                <i class="bi bi-file-text fs-1"></i>
-                            </div>
-                            <div class="solu_title">
-                                <h3>مدیریت فرم‌ها</h3>
-                            </div>
-                            <div class="solu_description">
-                                <p>
-                                    در این قسمت فرم مورد نظر خود را طراحی کنیدو فرم های ساخته شده را مشاهده کنید.
-                                </p>
-                                <button type="button" class="read_more_btn"
-                                    data-direct-url="<?php echo home_url('panel/formmaker') ?>" onclick="cardBtn(event)">ورود</button>
-                            </div>
-                        </div>
-                        <div class="solution_card">
-                            <div class="hover_color_bubble"></div>
-                            <div class="so_top_icon">
-                                <!-- Add icon here if needed -->
-                                <i class="bi bi-tools fs-1"></i>
-                            </div>
-                            <div class="solu_title">
-                                <h3>مدیریت تجهیزات</h3>
-                            </div>
-                            <div class="solu_description">
-                                <p>
-                                    در این قسمت شما می‌توانید تجهیزات را جستجو کنید اطلاعات را به روز رسانی کنید یا اطلاعات جدیدی برای هر تجهیز وارد کنید
+<?php
+global $wpdb;
 
-                                </p>
-                                <button type="button" class="read_more_btn"
-                                    data-direct-url="<?php echo home_url('panel/equipmenttracker') ?>" onclick="cardBtn(event)">ورود</button>
-                            </div>
-                        </div>
-                    </div>
-                    <!--  -->
-                    <div class="solution_cards_box sol_card_top_3">
-                        <div class="solution_card">
-                            <div class="hover_color_bubble">
+$user_id = get_current_user_id();
+$user_role = get_user_meta($user_id, '_role', true);
+$column_name = $user_role . '_id';
+$query = $wpdb->prepare("SELECT * FROM workflow WHERE {$column_name}=%d", $user_id);
+$workflow = $wpdb->get_results($query, ARRAY_A);
 
+?>
 
-                            </div>
-                            <div class="so_top_icon">
-                                <!-- Add icon here if needed -->
-                                <i class="bi bi-people fs-1"></i>
-                            </div>
-                            <div class="solu_title">
-                                <h3>مدیریت کاربران</h3>
-                            </div>
-                            <div class="solu_description">
-                                <p>
-                                    در این قسمت می توانید کاربر جدید را عضو کنید و نقش های کاربران را مدیریت کنید.
-                                </p>
-                                <button type="button" class="read_more_btn"
-                                    data-direct-url="<?php echo home_url('panel/manageuser') ?>" onclick="cardBtn(event)">ورود</button>
-                            </div>
-                        </div>
-                        <div class="solution_card">
-                            <div class="hover_color_bubble"></div>
-                            <div class="so_top_icon">
-                                <!-- Add icon here if needed -->
-                            </div>
-                            <div class="solu_title">
-                                <h3>دمو ۴</h3>
-                            </div>
-                            <div class="solu_description">
-                                <p>
-                                    این یک متن نمونه است که برای تست قالب استفاده می‌شود. متن اصلی در این قسمت قرار می‌گیرد.
-                                </p>
-                                <button type="button" class="read_more_btn">بیشتر بخوانید</button>
-                            </div>
-                        </div>
-                    </div>
+<div class="container">
+  <div
+    class="table-responsive">
+    <table
+      class="table table-striped table-hover table-borderless table-dark align-middle">
+      <thead class="table-light">
+        <caption>
+          گردش کار
+        </caption>
+        <tr class="text-center">
+          <th>ردیف</th>
+          <th>شماره سریال</th>
+          <th>وضعیت</th>
+          <th>نقش فعال</th>
+          <th>زمان ثبت</th>
+          <th>عملیات</th>
+        </tr>
+      </thead>
+      <tbody class="table-group-divider" id="workflow-table-body">
+        <?php if (empty($workflow)) {
+        ?>
+          <tr class="bg-light text-dark">
+            <td colspan="6" class="bg-light text-center fw-bold">
+              گردش کاری برای شما ثبت نشده است.
+            </td>
+          </tr>
+        <?php
+        } else {
+        ?>
+          <?php
+          $i = 1;
+          $status_persian = [
+            "Pending" => "انتظار تایید ناظر",
+            "SupervisorApproved" => "تایید توسط ناظر",
+            "SupervisorReject" => "رد توسط ناظر",
+            "FinalApprove" => "تایید توسط مدیر",
+            "ManagerReject" => "رد توسط مدیر",
+          ];
+
+          foreach ($workflow as $row) {
+
+          ?>
+            <tr
+              class="table-primary">
+              <td scope="row"><?php echo $i++ ?></td>
+
+              <td><?php echo $row['equipment_id'] ?></td>
+              <td><?php echo $status_persian[$row['current_status']] ?></td>
+              <td><?php echo $row['active_role'] ?></td>
+              <td
+                class="dateTime"
+                onclick=dateTimeFormatter(this)>
+                <?php echo $row['update_at'] ?></td>
+              <td>
+                <div class="d-grid gap-2">
+                  <button
+                    type="button"
+                    name=""
+                    id=""
+                    class="btn btn-primary">
+                    جزییات
+                  </button>
                 </div>
-            </div>
-        </div>
-    </div>
+              </td>
+            </tr>
+          <?php } ?>
+          <script>
+            document.addEventListener("DOMContentLoaded", dateTimeFormatter);
+
+            function dateTimeFormatter(e) {
+              const elements = document.querySelectorAll('.dateTime');
+              if (elements.length) {
+                elements.forEach(el => {
+                  const dateTime = el.textContent.trim()
+                  const dateTimeObj = new Date(dateTime);
+                  const persianDate = dateTimeObj.toLocaleDateString('fa-IR', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: false // Use 24-hour format
+                  })
+                  el.textContent = persianDate;
+                })
+              } else {
+                setTimeout(dateTimeFormatter, 100);
+              }
+            }
+          </script>
+
+
+        <?php } ?>
+      </tbody>
+      <tfoot>
+
+      </tfoot>
+    </table>
+  </div>
+
 </div>
-<script>
-    function cardBtn(event) {
-        const url = event.target.getAttribute('data-direct-url')
-        window.open(url, '_self');
-    }
-</script>
