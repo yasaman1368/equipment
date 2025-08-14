9359e491754fd855405203265fd1d3b01545839e
hossein aalipour <alipor.hossain@gmial.com>
2025-08-14 19:36:55 +0330

    ref: remove equipment from workflow and equiment_data tabel added

--- wp-content/themes/equipment/_inc/form-equipment/remove-equipment-data.php
+++ wp-content/themes/equipment/_inc/form-equipment/remove-equipment-data.php
@@ -2,22 +2,18 @@
 
 function remove_equipment_data()
 {
-    if (!isset($_POST['serial_number'])) {
-        wp_send_json_error(array('message' => 'serial_number is missing in the request.'));
-        return;
-    }
+  if (!isset($_POST['equipment_id'])) {
+    wp_send_json_error(array('message' => 'equipment_id is missing in the request.'));
+    return;
+  }
 
-    $serial_number = intval($_POST['serial_number']);
+  $equipment_id = sanitize_text_field($_POST['equipment_id']);
 
-    global $wpdb;
-    $table_name_equipments = $wpdb->prefix . 'equipments';
-    $table_name_equipment_data = $wpdb->prefix . 'equipment_data';
+  global $wpdb;
+  $table_name_equipments = $wpdb->prefix . 'equipments';
+  $wpdb->delete($table_name_equipments, ['equipment_id' => $equipment_id]);
 
-    $wpdb->delete($table_name_equipments, ['id' => $serial_number]);
-
-    $wpdb->delete($table_name_equipment_data, ['equipment_id' => $serial_number]);
-
-    wp_send_json_success(array('message' => 'تجهیز با موفقیت حذف شد'));
+  wp_send_json_success(array('message' => 'تجهیز با موفقیت حذف شد'));
 }
 
 add_action('wp_ajax_remove_equipment_data', 'remove_equipment_data');
