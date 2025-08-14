b877ab7f3e6f23da87a6b8040544213dd0754e6a
hossein aalipour <alipor.hossain@gmial.com>
2025-08-14 11:53:26 +0330

    debug: when get forms in a location debuged

--- wp-content/themes/equipment/_inc/utility/locations.php
+++ wp-content/themes/equipment/_inc/utility/locations.php
@@ -1,18 +1,17 @@
 <?php
 function get_form_locations($form_id)
 {
-    global $wpdb;
-    $table_name = $wpdb->prefix . "equipment_forms";
-    $locations = $wpdb->get_var($wpdb->prepare("SELECT locations FROM $table_name WHERE id = $form_id"));
-    if (!$locations) {
-        return false;
-    }
-
-    return json_decode($locations);
+  global $wpdb;
+  $table_name = $wpdb->prefix . "equipment_forms";
+  $locations = $wpdb->get_var($wpdb->prepare("SELECT locations FROM $table_name WHERE id=%d", $form_id));
+  if (!$locations) {
+    return false;
+  }
+  return json_decode($locations, true);
 }
 
 function get_user_locations($user_id)
 {
-    $user_locations = get_user_meta($user_id, '_locations', true) ?: [];
-    return $user_locations;
+  $user_locations = get_user_meta($user_id, '_locations', true) ?: [];
+  return $user_locations;
 }
